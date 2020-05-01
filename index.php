09d0be2d94a83feb1afbc6da09456e2a5fbba020
longthailai <longthailai@gmail.com>
2020-05-01 11:01:57 +0900

    Create new index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>LIFF Starter</title>
+        <link rel="stylesheet" href="./public/css/style.css">
+        <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
+        <script src="./public/js/liff-starter.js"></script>
+    </head>
+  <body>
+    <div>
+      <div><h2>Heroku | Welcome to your new app!</h2></div>
+      <div><p>Demo: Liff App</p></div>
+    </div>
+  </body>
+</html>
