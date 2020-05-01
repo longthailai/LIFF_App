4b8214b66f2f67ce1204a337fe3abb52baeb720b
longthailai <longthailai@gmail.com>
2020-05-01 11:37:47 +0900

    Update liff-starter.js

--- public/js/liff-starter.js
+++ public/js/liff-starter.js
@@ -1,2 +1,25 @@
 var myLiffId = '1654158517-n46wYMRg';
 var myLiffApp = 'line://app/' + myLiffId;
+
+
+/**
+* Initialize LIFF
+* @param {string} myLiffId The LIFF app ID of the selected element
+*/
+function initializeLiff(myLiffId) {
+    liff.init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            // start to use LIFF's api
+            initializeApp();
+        })
+        .catch((err) => {
+            document.getElementById("liffAppContent").classList.add('hidden');
+            document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+        });
+}
+
+$(document).ready(function() {
+  initializeLiff(myLiffId);
+});
